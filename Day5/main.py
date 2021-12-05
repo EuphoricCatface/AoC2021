ee61ea233f3db554da774946f2188fe16aa13f01
Seongyong Park <EuphoricCatface@gmail.com>
2021-12-05 22:26:10 +0900

    delete unnecessary lines

--- Day5/main.py
+++ Day5/main.py
@@ -9,64 +9,10 @@ BOARD_DIMENSION_TYPE = tuple[tuple[int, int], tuple[int, int]]
 DEBUG = True
 
 
-def filter_lines(line_txt: str) -> list[LINE_TYPE]:
-    line_txt_list = line_txt.split("\n")
-
-    invalid_lines = 0
-    dot_count = 0  # probably should be counted
-    vertical_count = 0
-    horizontal_count = 0
-    slanted_count = 0  # will be ignored
-
-    filtered_list = []
-
-    print(f"total lines: {len(line_txt_list)}")
-
-    for ln_num, line in enumerate(line_txt_list):
-        elements = line.split(' ')  # tuple, ->, tuple
-        if len(elements) != 3:
-            print(f"Wrong Input{ln_num}: {line}")
-            continue
-
-        coord1: tuple = ast.literal_eval(elements[0])
-        coord2: tuple = ast.literal_eval(elements[2])
-
-        # I mean, I saw the data and there weren't "wrong input" inside :d
-        # if issubclass(tuple, coord1.__class__) or \
-        #         issubclass(tuple, coord2.__class__):
-        #     print(f"Wrong Input{ln_num}: {line}")
-        #     invalid_lines = invalid_lines + 1
-        #     continue
-
-        coord_tuple = (coord1, coord2)
-        filtered_list.append(coord_tuple)
-        # will be pop()-ed if it turns out to be slanted
-
-        if coord1 == coord2:
-            dot_count = dot_count + 1
-            continue
-
-        if coord1[0] == coord2[0]:
-            vertical_count = vertical_count + 1
-            continue
-
-        if coord1[1] == coord2[1]:
-            horizontal_count = horizontal_count + 1
-            continue
-
-        slanted_count = slanted_count + 1
-        filtered_list.pop()
-
-    print(f"{invalid_lines=}, {dot_count=}, {vertical_count=}, {horizontal_count=}, {slanted_count=}")
-
-    return filtered_list
-
-
 def filter_lines_hv45(line_txt: str) -> tuple[list[LINE_TYPE], list[LINE_TYPE], list[LINE_TYPE]]:
     line_txt_list = line_txt.split("\n")
 
     invalid_lines = 0
-    # dot_count = 0  # probably should be counted
     horizontal_count = 0
     vertical_count = 0
     slanted45_count = 0
@@ -90,11 +36,6 @@ def filter_lines_hv45(line_txt: str) -> tuple[list[LINE_TYPE], list[LINE_TYPE], 
         coord2: tuple = ast.literal_eval(elements[2])
 
         coord_tuple = (coord1, coord2)
-        # filtered_list.append(coord_tuple)
-
-        # if coord1 == coord2:
-        #     dot_count = dot_count + 1
-        #     continue
 
         if coord1[1] == coord2[1]:
             horizontal_count = horizontal_count + 1
@@ -113,7 +54,6 @@ def filter_lines_hv45(line_txt: str) -> tuple[list[LINE_TYPE], list[LINE_TYPE], 
             continue
 
         slanted_odd_count = slanted_odd_count + 1
-        # filtered_list.pop()
 
     print(f"{invalid_lines=}, {vertical_count=}, {horizontal_count=}, {slanted45_count=}, {slanted_odd_count=}")
 
@@ -150,22 +90,6 @@ def board_dimension(lines: list[LINE_TYPE]) -> BOARD_DIMENSION_TYPE:
     return (x_min, y_min), (x_max, y_max)
 
 
-def place_lines_and_check(dim: BOARD_DIMENSION_TYPE, lines: list[LINE_TYPE]):
-    grand_total: int = 0
-    board: list
-
-    width = dim[1][0] - dim[0][0]
-    height = dim[1][1] - dim[0][1]
-
-    row = [0 for _ in range(width)]
-    board = [row[:] for _ in range(height)]
-    # row[:] - avoid shallow copy
-
-    # sheesh, we need to test vertical or horizontal now
-    # maybe not. let's try rearranging filtered_lines
-    raise NotImplementedError
-
-
 def place_lines_and_check_hv(dim: BOARD_DIMENSION_TYPE, v_lines: list[LINE_TYPE], h_lines: list[LINE_TYPE]):
     grand_total: int = 0
     board: list
@@ -188,10 +112,6 @@ def place_lines_and_check_hv(dim: BOARD_DIMENSION_TYPE, v_lines: list[LINE_TYPE]
         increment = int(increasing) * 2 - 1  # -1 or 1
         advance = (0, increment)
 
-        # start = h_line[0]
-        # end = h_line[1]
-        # Example works like this because it covers all board.
-        # We need to subtract dim[0] which is minimum value of the board
         start = tuple(map(operator.sub, h_line[0], dim[0]))
         end = tuple(map(operator.sub, h_line[1], dim[0]))
 
@@ -215,10 +135,6 @@ def place_lines_and_check_hv(dim: BOARD_DIMENSION_TYPE, v_lines: list[LINE_TYPE]
         increment = int(increasing) * 2 - 1  # -1 or 1
         advance = (increment, 0)
 
-        # start = v_line[0]
-        # end = v_line[1]
-        # Example works like this because it covers all board.
-        # We need to subtract dim[0] which is minimum value of the board
         start = tuple(map(operator.sub, v_line[0], dim[0]))
         end = tuple(map(operator.sub, v_line[1], dim[0]))
 
@@ -244,15 +160,12 @@ def place_lines_and_check_hv(dim: BOARD_DIMENSION_TYPE, v_lines: list[LINE_TYPE]
 if __name__ == '__main__':
     data_ = sys.stdin.read()
 
-    # filtered_lines: list[LINE_TYPE]
     filtered_lines_h: list[LINE_TYPE]
     filtered_lines_v: list[LINE_TYPE]
     filtered_lines_45:list[LINE_TYPE]
     board_dimensions: BOARD_DIMENSION_TYPE
 
-    # filtered_lines = filter_lines(data_)
     filtered_lines_h, filtered_lines_v, filtered_lines_45 = filter_lines_hv45(data_)
-    # board_dimensions = board_dimension(filtered_lines)
     raise NotImplementedError
     board_dimensions = board_dimension(filtered_lines_h + filtered_lines_v)
 
